231b9c08b3e2ff168a61716e40932bb306e1fba8
potatorainbow <mae.ngo@warriorlife.net>
2017-12-13 10:54:38 -0800

    Buttons
    - moved functions day and night to the javascript file
    
    - deleted functions day and night from html file
    - deleted 'script' tags from html file

--- calculator.js
+++ calculator.js
@@ -4,6 +4,16 @@ function d(val) {
 	document.getElementById("d").value = val;
 }
 
+//Buttons for Modes
+function night() {
+	document.body.style.backgroundColor= "black";
+	
+}
+
+function day() {
+	document.body.style.backgroundColor= "white";	
+}
+
 //Type numbers and operators
 function v(val) {
 	document.getElementById("d").value += val;
