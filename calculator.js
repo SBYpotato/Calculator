ed82fbab52cf7753abed34e95f6f0dda9adfcf9e
potatorainbow <mae.ngo@warriorlife.net>
2017-12-15 10:37:48 -0800

    The Beginning of a Miracle
    - i literally just found out how to change the button color

--- calculator.js
+++ calculator.js
@@ -4,14 +4,6 @@ function d(val) {
 	document.getElementById("d").value = val;
 }
 
-function night() {
-	document.body.style.backgroundColor= "black";
-}
-
-function day() {
-	document.body.style.backgroundColor = "white";
-}
-
 //Type numbers and operators
 function v(val) {
 	document.getElementById("d").value += val;
@@ -25,4 +17,23 @@ function e() {
 	catch(err) {
 		d("Error");
 	}
+}
+
+function day() {
+	document.body.style.backgroundColor= "white";
+	document.body.style.color="white";
+		document.getElementById("1").style.backgroundColor="red";
+		document.getElementById("1a").style.backgroundColor="red";
+		document.getElementById("1b").style.backgroundColor="red";
+		document.getElementById("1c").style.backgroundColor="red";
+
+}
+
+function night() {
+	document.body.style.backgroundColor= "black";
+	document.body.style.color="white";
+		document.getElementById("1").style.backgroundColor="orange";
+		document.getElementById("1a").style.backgroundColor="orange";
+		document.getElementById("1b").style.backgroundColor="orange";
+		document.getElementById("1c").style.backgroundColor="orange";
 }
